f6b4511f811150af3467e2d20c14b5b6baf6ee87
raphaelCoelhoDeLima <rcl.lp@hotmail.com>
2022-01-17 22:56:23 -0300

    Separando por dia

---
+++ componentes/carregaTarefa.js
@@ -0,0 +1,16 @@
+import { criaData } from "./criaData.js"
+
+
+export const carregaTarefa =() =>{
+    const lista = document.querySelector("[data-list]")
+    const tarefasCadastradas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+    lista.innerHTML = " "
+    tarefasCadastradas.forEach((tarefa) =>{
+        const dia = moment(tarefa.dataFormatada, "DD/MM/YYYY")
+
+       
+        lista.appendChild(criaData(dia))
+        
+    })
+}
--- componentes/criaTarefa.js
+++ componentes/criaTarefa.js
@@ -1,3 +1,4 @@
+import { carregaTarefa } from "./carregaTarefa.js"
 import BotaoConclui from "./concluiTarefa.js"
 import BotaoDelata from "./deletaTarefa.js"
 
@@ -6,7 +7,7 @@ export const handleNovoItem = (evento) =>{
 
     evento.preventDefault()
     const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
-    const lista = document.querySelector("[data-list]")
+   
     const input = document.querySelector("[data-form-input]")
     const valor = input.value 
     const calendario = document.querySelector("[data-form-date]")
@@ -19,15 +20,12 @@ export const handleNovoItem = (evento) =>{
     }
 
     const tarefasAtualizadas = [...tarefas, dados]
-    const criaTarefa = Tarefa(dados)
-    lista.appendChild(criaTarefa)
-
-   
 
     localStorage    .setItem("tarefas", JSON.stringify(tarefasAtualizadas))
 
     input.value = " "
 
+    carregaTarefa()
 }
 
 export const Tarefa = ({valor, dataFormatada}) =>{
